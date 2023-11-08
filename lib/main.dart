7e42ffdcf4ba459a8fd7d33c15bf619c0a442a78
Anthonyx578 <antrogartme2002@gmail.com>
2023-11-08 18:15:20 -0500

    Trabajo de tarea

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:emails/screens/ListScreen.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tu App',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: const ListScreen(),
+    );
+  }
+}
